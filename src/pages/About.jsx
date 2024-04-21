08f5977a354ad644d383a9bc00fa88bc87736369
Sumit Ojha <102379532+sumitojha2002@users.noreply.github.com>
2024-04-21 20:30:28 +0545

    edited about.js

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-  return (
-    <div>
-      About
-    </div>
-  )
+  return <div>Abou</div>;
 }
 
-export default About
+export default About;
